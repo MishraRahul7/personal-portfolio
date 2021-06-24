076e5be5ada804416d15d83830af6d402503a68e
Rahul Mishra <49086503+MishraRahul7@users.noreply.github.com>
2021-06-24 12:28:16 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -44,6 +44,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "1vh",
     color: "#fff",
     fontSize: "1rem",
+    width:"70vw"
     [theme.breakpoints.down("xs" | "sm")]: {
       width: "100vw",
     },
